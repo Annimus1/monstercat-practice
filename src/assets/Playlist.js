40e69764dc06beaa5af2047392e2e38adc44038d
pablo <pablo.j98@gmail.com>
2024-07-23 14:56:42 -0400

    update song resources

--- src/assets/Playlist.js
+++ src/assets/Playlist.js
@@ -7,67 +7,67 @@ const data = [
         id:1,
         name:'Radioactive',
         length:'3:06',
-        source:'1pv47u9l-KbTMyhpfWxmKFqK8XLFjy83g'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/radioactive.mp3?alt=media&token=49e2f67b-56f9-41d4-93e8-b4148e8ce399'
       },
       {
         id:2,
         name:'Tiptoe',
         length:'3:13',
-        source:'1KVDdvkaVkXK8r1T5T9O-9hFCDOGDBGtu'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/tiptoe.mp3?alt=media&token=f6890571-0e98-4710-8fc5-f0b16f253c59'
       },
       {
         id:3,
         name:"It's Time",
         length:'3:57',
-        source:'1_gQWLREto-IXaJQSXySJXtMSds1OS2Li'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/it_s%20time.mp3?alt=media&token=7fefa0a1-55d9-424a-8ddd-430c067aacdf'
       },
       {
         id:4,
         name:'Demons',
         length:'2:55',
-        source:'1A0M6JininBDrGpp5Xj2qSLp34Uze4wrP'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/demons.mp3?alt=media&token=4b7595e8-012c-4dd4-a44b-6b3f62ba2a84'
       },
       {
         id:5,
         name:'On Top of the World',
         length:'3:09',
-        source:'1CgHibwkqrQglzEVuCyiAXc7AXnJ9qifW'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/on%20top%20of%20the%20world.mp3?alt=media&token=60e9aeb5-c494-4abc-a2ae-5ee4f6bc2cd3'
       },
       {
         id:6,
         name:'Amsterdam',
         length:'4:01',
-        source:'1OB6Ccy_j3Yc19rcDVyNXCuOj5A1hMpwB'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/amsterdam.mp3?alt=media&token=17cbc433-40d3-4ba0-be68-f3a0b90d6169'
       },
       {
         id:7,
         name:'Hear Me',
         length:'3:52',
-        source:'1Gmvcq5vvEMHSJGd_n4e-QOwp27Vl404e'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/hear%20me.mp3?alt=media&token=87dbed14-f81e-4903-9663-a63986774159'
       },
       {
         id:8,
         name:'Every Night',
         length:'3:35',
-        source:'1qVm3zR0lIecYn9kvrSSDU52KG8RAJH6A'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/every%20night.mp3?alt=media&token=9c3806ca-f757-4b5a-a3b2-fbeab685c7ac'
       },
       {
         id:9,
         name:'Bleeding Out',
         length:'3:41',
-        source:'1R9x71mfG4He6cf6_P5g2FDDHaAEmmeyu'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/bleeding%20out.mp3?alt=media&token=750a69e7-f150-4766-809b-0fef2c8bd4e7'
       },
       {
         id:10,
         name:'Underdog',
         length:'3:26',
-        source:'12hH1yF-g_pZzeytrfYKXNh9RYJPhtpaj'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/underdog.mp3?alt=media&token=ca22af5e-da8a-4170-9080-406b7ce35792'
       },
       {
         id:11,
         name:'Nothing Left To Say',
         length:'5:44',
-        source:'1ChexFVnIjLa1kO10q2MRzxZBsRxz5adC'
+        source:'https://firebasestorage.googleapis.com/v0/b/monstercat-practice.appspot.com/o/nothing%20left%20to%20say.mp3?alt=media&token=c9767240-6cd2-465a-a0d2-446a6a41ae92'
       }            
     ]
   }
