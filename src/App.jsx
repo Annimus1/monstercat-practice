593d48d4f9b97217261e1cbc35d032095141bfd1
Pablo Vergara <pablo.j98@gmail.com>
2025-09-27 18:48:58 -0400

    Update: Cover
    Fix Background image.
    Fix cover image.
    Fix typing Error.

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,18 @@ import Header from "./components/header";
 import Newsletter from "./components/Newsletter";
 import Footer from "./Footer";
 
-// reference https://www.frontendpractice.com/projects/monstercat
-
 function App() {
-  const imgUrl = 'https://th.bing.com/th/id/R.8863a420e0de711cf4d2286b14ca70a0?rik=CLF5QdUigGbwfg&riu=http%3a%2f%2fimg2-ak.lst.fm%2fi%2fu%2far0%2fecae82853b784726c7e2c4e2ba55a4fd&ehk=OtDvAyDAYkqsGWv5xFhZAPrPjdfUz4S5KOB8TGxcOr4%3d&risl=&pid=ImgRaw&r=0'
-
   const bgImage = {
-    backgroundImage:`url(${imgUrl})`, 
-    backgroundRepeat:"no-repeat" , 
-    objectFit:"contain",
-    backgroundSize: '100%',
-    backgroundPositionX: 'center'
+    backgroundImage:`url('/Night Visions.jpg')`,
+    backgroundRepeat:"no-repeat" ,
+    backgroundSize: "contain",
+    backgroundPositionX: "center"
   }
 
   return (
     <div  className="min-w-screen min-h-screen w-full h-full overflow-x-hidden overflow-y-hidden relative bg-black">
       <div style={bgImage} className="bg-black" >
-        <div className="z-10 h-full backdrop-blur-md">
+        <div className="z-10 h-auto backdrop-blur-md">
           
           {/* Header */}
           <Header/>
--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,11 +1,9 @@
 import { useState, useEffect, useContext } from "react"
 import data from "../assets/Playlist";
 import { songContext } from "../context/SongContext";
-
+import imgUrl from '../../public/Night Visions.jpg'
 
 function Cover() {
-  
-  const imgUrl = 'https://th.bing.com/th/id/R.8863a420e0de711cf4d2286b14ca70a0?rik=CLF5QdUigGbwfg&riu=http%3a%2f%2fimg2-ak.lst.fm%2fi%2fu%2far0%2fecae82853b784726c7e2c4e2ba55a4fd&ehk=OtDvAyDAYkqsGWv5xFhZAPrPjdfUz4S5KOB8TGxcOr4%3d&risl=&pid=ImgRaw&r=0'
   const [playing, setPlaying] = useState(false);
   const { currentSong } = useContext(songContext);
 
@@ -29,7 +27,8 @@ function Cover() {
   }, [])
 
   return (
-    <section className='text-white h-full w-full px-8 mt-32 pb-2 md:grid md:grid-cols-2 bg-gradient-to-b from-transparent to-black'>
+    <section className='text-white h-full w-full px-8 mt-32 pb-2 md:grid md:grid-cols-2 
+      bg-gradient-to-b from-transparent to-black'>
 
       <div className="w-full h-auto">
         <img
@@ -47,8 +46,8 @@ function Cover() {
       <div className="md:flex md:flex-col md:h-auto md:mx-auto md:justify-center lg:mx-0">
 
         <div className=" md:pb-20">
-          <h1 className="font-[Raleway] text-4xl lg:text-6xl md:text-4xl font-semibold mt-5 mb-2">Night Visionas</h1>
-          <span className="font-[Raleway] text-xl font-medium text-green-500">Imagine Dragons</span>
+          <h1 className="font-[Raleway] text-4xl lg:text-6xl md:text-4xl font-semibold mt-5 mb-2">{data[0].album}</h1>
+          <span className="font-[Raleway] text-xl font-medium text-green-500">{data[0].artist}</span>
         </div>
 
         <div className=" mt-6 pb-6 flex flex-col md:block gap-4">
